69ef0edc2c0b813d99c4bc5febe54db348a2670f
ShirlyyJ <shirleyj13@nycstudents.net>
2024-11-21 10:05:18 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 800
+screen_height = 600 
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Game")
