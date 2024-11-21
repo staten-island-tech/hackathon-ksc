f516d5b1c1cdf4fe8c26788b046adf3cc21d7324
ShirlyyJ <shirleyj13@nycstudents.net>
2024-11-21 10:15:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import pygame
-import sys
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600 
+screen_width = 1200
+screen_height = 800
 
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Game")
+
+running = True 
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+screen.fill((235, 192, 230))
