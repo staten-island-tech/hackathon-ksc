a447a8f6a059590e68dc60713dc7ae046fc43cf1
ShirlyyJ <shirleyj13@nycstudents.net>
2024-11-25 10:14:28 -0500

    xx

--- app.py
+++ app.py
@@ -5,18 +5,15 @@ screen_width = 1200
 screen_height = 800
 
 screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption("Music game")
+pygame.display.set_caption("Dinosaur game")
+background = pygame.image.load('downloads/pixlebg.jpg')
 
 player_width = 50
 player_height = 50
 player_x = screen_width // 2
 player_y = screen_height - player_height - 10
-player_speed = 5
+player_speed = 1
 
-note_width = 30
-note_height = 30
-note_speed = 5
-notes = []
 
 running = True 
 while running:
@@ -24,14 +21,14 @@ while running:
         screen.fill((235, 192, 230))
         if event.type == pygame.QUIT:
             running = False
-
-keys = pygame.key.get_pressed()
-if keys[pygame.K_LEFT] and player_x > 0:
-    player_x -= player_speed
-if keys[pygame.K_RIGHT] and player_x < screen_width - player_width:
-    player_x += player_speed
-
-
     
-screen.fill((100, 0, 0))
-pygame.display.update()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_x > 0:
+        player_x -= player_speed
+    if keys[pygame.K_RIGHT] and player_x < 0:
+        player_x += player_speed
+            
+    pygame.draw.rect(screen, (0, 0, 255), (player_x, player_y, player_width, player_height))
+    
+    pygame.display.update()
+pygame.quit()
