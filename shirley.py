5e503a250e911385cd81e6067ad3cc3b0dcad4c1
shirley ♡ <shirleyj13@nycstudents.net>
2024-11-26 17:36:20 -0500

    xx

--- shirley.py
+++ shirley.py
@@ -5,38 +5,44 @@ screen_width = 1200
 screen_height = 800
 
 screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption("Dinosaur game")
+pygame.display.set_caption("Jumping game")
 
 player_x = 100
 player_y = 300
 player_width = 50
 player_height = 50
 jumping = False
+falling = False
 
 obstacle_x = 800
 obstacle_y = 330
 obstacle_width = 30
 obstacle_height = 30
+jump_count = 20
 
 running = True 
 while running:
+    screen.fill((235, 192, 230))
     for event in pygame.event.get():
-        screen.fill((235, 192, 230))
         if event.type == pygame.QUIT:
             running = False
     
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE] and not jumping:
+    if keys[pygame.K_SPACE] and not jumping and not falling:
         jumping = True
-            
-    elif jumping:
+        jump_count = 20
+    if jumping:
         player_y -= 5  
-        if player_y <= 200:  
+        jump_count -= 1  
+        if jump_count <= 0: 
             jumping = False
-    else:
-        if player_y < 300:  
-            player_y += 5
-    
+            falling = True
+    if falling:
+        player_y += 5
+        if player_y >= 300:  
+            player_y = 300
+            falling = False
+
     obstacle_x -= 5
     if obstacle_x < -30: 
         obstacle_x = 800
@@ -44,7 +50,7 @@ while running:
     if player_x < obstacle_x + obstacle_width and player_x + player_width > obstacle_x and player_y + player_height > obstacle_y:
         running = False
         
-    screen.fill((235, 192, 230))  
+        
     pygame.draw.rect(screen, (0, 0, 255), (player_x, player_y, player_width, player_height))
     pygame.draw.rect(screen, (255, 0, 0), (obstacle_x, obstacle_y, obstacle_width, obstacle_height))
 
