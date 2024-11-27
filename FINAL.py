87ec89e4a3e7edfd87358c98a1dd9f2834bb838f
shirley ♡ <shirleyj13@nycstudents.net>
2024-11-26 22:14:55 -0500

    xxx

--- FINAL.py
+++ FINAL.py
@@ -4,34 +4,32 @@ pygame.init()
 screen_width = 1200
 screen_height = 800
 
-screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.Sdisplay.set_caption("KSC game")
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("KSC game") 
 
-background = pygame.image.load
+background = pygame.image.load('/Users/shirleyjiang/Documents/GitHub/hackathon-ksc/pixel-background.jpg')
 background = pygame.transform.scale(background, (screen_width, screen_height))
 
 jumping = False
 falling = False
 
 oval1_x = 100
-oval1_y = 300
+oval1_y = 600  
 oval1_width = 120
 oval1_height = 80
-oval1_surface = pygame.Surface((oval1_width, oval1_height), pygame.SRCALPHA)
-oval2_x = 1010
-oval2_y = 275
+oval1_color = (125, 77, 31)
+
 oval2_width = 100
 oval2_height = 60
-oval2_surface = pygame.Surface((oval2_width, oval2_height), pygame.SRCALPHA)  # Use SRCALPHA for transparency
 oval2_color = (125, 77, 31)
-pygame.draw.ellipse(oval2_surface, oval2_color, (0, 0, oval2_width, oval2_height))
-
 
 obstacle_x = 800
-obstacle_y = 330
+obstacle_y = 625
 obstacle_width = 30
 obstacle_height = 30
-jump_count = 20
+obstacle_color = (255, 255, 255)
+
+jump_count = 30
 
 running = True 
 while running:
@@ -40,30 +38,33 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(background, (0, 0))  # Blit the background image
-    screen.blit(oval1_surface, (oval1_x, oval1_y))
+    screen.blit(background, (0, 0))  
+
+    pygame.draw.ellipse(screen, oval1_color, (oval1_x, oval1_y, oval1_width, oval1_height))
+    
+    oval2_y = oval1_y - oval2_height + 20  
+    pygame.draw.ellipse(screen, oval2_color, (oval1_x + 10, oval2_y, oval2_width, oval2_height))
+
+    pygame.draw.rect(screen, obstacle_color, (obstacle_x, obstacle_y, obstacle_width, obstacle_height))
+
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE] and not jumping and not falling:
         jumping = True
-        jump_count = 20
+        jump_count = 30
     if jumping:
-        oval1_y -= 5
-        oval2_y -= 5
+        oval1_y -= 5  
         jump_count -= 1
         if jump_count <= 0:
             jumping = False
             falling = True
     if falling:
-        oval1_y += 5
-        oval2_y += 5
-        if oval1_y >= 300:
-            oval1_y = 300
-            oval2_y = 300
+        oval1_y += 5  
+        if oval1_y >= 600:  
+            oval1_y = 600
             falling = False
-
     obstacle_x -= 5
-    if obstacle_x < -30: 
+    if obstacle_x < -30:
         obstacle_x = 800
         
         
